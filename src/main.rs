d9053b8627ff56c770f248d9bd026c299859f701
Malik Olivier Boussejra <malik@boussejra.com>
2018-09-21 22:15:02 +0900

    Detect if terminal is TTY
    If terminal is not a tty, there is no need to use less, so just write all
    the output to stdout.

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ extern crate clap;
 extern crate csv;
 #[macro_use]
 extern crate lazy_static;
+extern crate libc;
 
 mod cjk;
 
@@ -14,39 +15,49 @@ use std::process::{self, Command, Stdio};
 use clap::{App, Arg};
 
 fn main() -> Result<(), Box<Error>> {
+    let istty = unsafe { libc::isatty(libc::STDOUT_FILENO as i32) } != 0;
+    if istty {
+        let mut less = Command::new("less")
+            .stdin(Stdio::piped())
+            .arg("-S")
+            .spawn()
+            .expect("Could not run less!");
+
+        {
+            let less_in = less.stdin.as_mut().unwrap();
+            run(less_in)?;
+        }
+
+        less.wait().expect("Failed to wait on child");
+    } else {
+        let stdout = io::stdout();
+        let mut handle = stdout.lock();
+        run(&mut handle)?;
+    }
+
+    Ok(())
+}
+
+fn run<W: io::Write>(out: &mut W) -> Result<(), Box<Error>> {
     let matches = App::new(env!("CARGO_PKG_NAME"))
         .version(env!("CARGO_PKG_VERSION"))
         .author(env!("CARGO_PKG_AUTHORS"))
         .about(env!("CARGO_PKG_DESCRIPTION"))
         .arg(Arg::with_name("INPUT").help("Sets the csv file to read"))
         .get_matches();
-
-    let mut less = Command::new("less")
-        .stdin(Stdio::piped())
-        .arg("-S")
-        .spawn()
-        .expect("Could not run less!");
-
-    {
-        let less_in = less.stdin.as_mut().unwrap();
-
-        if let Some(input_file) = matches.value_of("INPUT") {
-            match File::open(input_file) {
-                Ok(mut f) => read_csv(&mut f, less_in)?,
-                Err(e) => {
-                    eprintln!("Error opening file '{}': {}", input_file, e);
-                    process::exit(1)
-                }
+    if let Some(input_file) = matches.value_of("INPUT") {
+        match File::open(input_file) {
+            Ok(mut f) => read_csv(&mut f, out),
+            Err(e) => {
+                eprintln!("Error opening file '{}': {}", input_file, e);
+                process::exit(1)
             }
-        } else {
-            let mut stdin = io::stdin();
-            let mut handle = stdin.lock();
-            read_csv(&mut handle, less_in)?
-        };
+        }
+    } else {
+        let mut stdin = io::stdin();
+        let mut handle = stdin.lock();
+        read_csv(&mut handle, out)
     }
-
-    less.wait().expect("Failed to wait on child");
-    Ok(())
 }
 
 fn read_csv<R: io::Read, W: io::Write>(buf: &mut R, out: &mut W) -> Result<(), Box<Error>> {
