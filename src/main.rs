adbc213ca8f71aae59fa920ae151427caa4d9c61
Malik Olivier Boussejra <malik@boussejra.com>
2018-09-21 11:17:14 +0900

    INIT

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+extern crate clap;
+extern crate csv;
+
+use clap::App;
+
+fn main() {
+    let matches = App::new(env!("CARGO_PKG_NAME"))
+        .version(env!("CARGO_PKG_VERSION"))
+        .author(env!("CARGO_PKG_AUTHORS"))
+        .about(env!("CARGO_PKG_DESCRIPTION"))
+        .get_matches();
+    println!("{:?}", matches);
+}
